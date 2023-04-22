401b35ac02e484e2dfec540878255d81ae2deb2c
Pranav Puranam <pranav.puranam@gmail.com>
2023-04-21 23:19:23 -0700

    add vegetable and product tables

--- models.py
+++ models.py
@@ -38,3 +38,52 @@ class User(db.model):
     
     def __repr__(self):
         return "<User ID: {}, Full Name: {}, Revenue: {}, Time Created: {}>".format(id, name, revenue, created_at)
+
+class Products(db.model):
+    __tablename__ = "user_products"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    owner = db.Column(db.Integer, db.ForeignKey("active_users.id"))
+    date_ready = db.Column(db.DateTime, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, owner, date_created, date_ready):
+        self.owner = owner
+        self.date_created = date_created
+        self.date_ready = date_ready
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return "".format()
+
+
+class Vegetable(db.model):
+    __tablename__ = "vegetables"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50))
+    season = db.Column(db.String(50))
+    temperature = db.Column(db.String(50))
+    soil_type = db.Column(db.String(50))
+
+    def __init__(self, name, season, temperature, soil_type):
+        self.name = name
+        self.season = season
+        self.temperature = temperature
+        self.soil_type = soil_type
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr(self):
+        pass
