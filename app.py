b9bbf79cc4896b7a2d0e9b0efdf93b784e043f85
Pranav Puranam <pranav.puranam@gmail.com>
2023-04-22 01:44:25 -0700

    change to firestore db

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-import os, re, sys
+import os, re, sys, random
 from dotenv import load_dotenv
+from datetime import datetime
 from flask import Flask, render_template, request, jsonify, url_for, redirect
 from flask_cors import CORS, cross_origin
-from flask_sqlalchemy import SQLAlchemy
+from models import Vegetable, Product, User
 from config import Config
+from firebase_admin import credentials, firestore, initialize_app
+
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config.from_object(Config) # TODO: add config.py file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app, support_credentials=True)
+cred = credentials.Certificate('./key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+
+# (Tables)
+vegetables = db.collection("vegetables")
+products = db.collection("user_products")
+users = db.collection("users")
+rand_set = set()
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -23,9 +35,48 @@ def test():
     """
     pass
 
+@app.route('/addUser', methods=["GET", "POST"])
+def add_user():
+    if request.method == "POST":
+        id = -1
+        while id < 0 or id in rand_set:
+            id = random.randint(1, 150000)
+        rand_set.add(id)
+        name = request.form["name"]
+        revenue = 0
+        if "revenue" in request.form:
+            revenue = request.form["revenue"]
+        location = request.form["location"]
+        created_at = datetime.now().date().strftime('%d-%m-%y')
+        user = User(id, name, revenue, location, created_at)
+        users.document(str(id)).set(user.to_dict())
+    return jsonify(user.to_dict())
 
+@app.route('/addVegetable', methods=["GET", "POST"])
+def add_vegetable():
+    if request.method == "POST":
+        name = request.form["name"]
+        season = request.form["season"]
+        temperature = request.form["temperature"]
+        soil_type = request.form["soil_type"]
+        veggie = Vegetable(name, season, season, soil_type)
+        vegetables.document(name).set(veggie.to_dict())
+    return jsonify(veggie.to_dict())
 
-
+@app.route('/addProduct', methods=["GET", "POST"])
+def add_product():
+    if request.method == "POST":
+        owner_id = request.form["owner_id"]
+        type = request.form["type"]
+        date_created = request.form["date_created"]
+        date_ready = request.form["date_ready"]
+        # first query and see if the owner exists, only then add
+        query = users.where(u'id', u'==', owner_id).stream()
+        product = Product(owner_id, type, date_created, date_ready)
+        if len(query) != 0:
+            products.document(type).set(product.to_dict())
+            return jsonify(product.to_dict())
+    return jsonify({})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(threaded=True, debug=True, port=os.environ.get('PORT'))
--- models.py
+++ models.py
@@ -1,89 +1,40 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-"""
-User:
-- Still growing / waiting to be sold
-- current plants that they are growing
-- ID
-- Revenue
-
-Vegetable Table (ID to check all vegetables for a specific user)
-
-Vegetables (Data for Processing):
-- Location
-- Soil
-- All that
-"""
-
-class User(db.model):
-    __tablename__ = "active_users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    revenue = db.Column(db.Integer, nullable=True)
-    created_at = db.Column(db.DateTime)
-
-    def __init__(self, name, revenue=0):
+class User(object):
+    def __init__(self, id, name, revenue=0, location="", created_at=None):
+        self.id = id
         self.name = name
         self.revenue = revenue
+        self.location = location
+        self.created_at = created_at
 
-    def add(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-    
+    def to_dict(self):
+        return {"name": self.name, "revenue": self.revenue, "location": self.location, "created_at": self.created_at}
+        
     def __repr__(self):
         return "<User ID: {}, Full Name: {}, Revenue: {}, Time Created: {}>".format(id, name, revenue, created_at)
 
-class Products(db.model):
-    __tablename__ = "user_products"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    owner = db.Column(db.Integer, db.ForeignKey("active_users.id"))
-    date_ready = db.Column(db.DateTime, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False)
-
-    def __init__(self, owner, date_created, date_ready):
-        self.owner = owner
+class Product(object):
+    def __init__(self, owner_id, type, date_created, date_ready):
+        self.owner_id = owner_id
+        self.type = type
         self.date_created = date_created
         self.date_ready = date_ready
 
-    def add(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+    def to_dict(self):
+        return {"owner": self.owner, "type": self.type, "date_created": self.date_created, "date_ready": self.date_ready}
     
     def __repr__(self):
         return "".format()
 
 
-class Vegetable(db.model):
-    __tablename__ = "vegetables"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50))
-    season = db.Column(db.String(50))
-    temperature = db.Column(db.String(50))
-    soil_type = db.Column(db.String(50))
-
+class Vegetable(object):
     def __init__(self, name, season, temperature, soil_type):
         self.name = name
         self.season = season
         self.temperature = temperature
         self.soil_type = soil_type
 
-    def add(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
+    def to_dict(self):
+        return {"name": self.name, "season": self.season, "temperature": self.temperature, "soil_type": self.soil_type}
+        
     def __repr(self):
         pass
